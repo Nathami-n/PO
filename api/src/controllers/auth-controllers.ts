9825b2d2f4546167d6b4ab1a2d3beec42cdca2ea
Nathami-n <georgenathan010@gmail.com>
2024-06-01 22:45:06 +0300

    Added the refresh token in the cookies

--- api/src/controllers/auth-controllers.ts
+++ api/src/controllers/auth-controllers.ts
@@ -1,125 +1,175 @@
-import type {Request, Response} from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import type { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config({
-    path: "./.env"
+  path: "./.env",
 });
 
-import { AdminModel, createUser, getUserByEmail } from '../model/user-models';
+import { AdminModel, createUser, getUserByEmail } from "../model/user-models";
 
 export const createAdmin = async (req: Request, res: Response) => {
-    const {
-        email,
-        name,
-        password
-    } = req.body;
+  const { email, name, password } = req.body;
 
-    if(!email || !name || !password) {
-        return res.json({success: false, data: {body: null, error: "Missing credentials"}});
+  if (!email || !name || !password) {
+    return res.json({
+      success: false,
+      data: { body: null, error: "Missing credentials" },
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const userData = {
+    email,
+    name,
+    hashedPassword,
+    authentication: { refreshToken: "" },
+  };
+
+  //check whether user exists
+  const isUserRegistered = await getUserByEmail("Admin", email);
+  if (isUserRegistered || isUserRegistered !== null) {
+    return res.json({
+      success: false,
+      data: { body: null, error: "Already registered" },
+    });
+  }
+
+  //save user to database
+  try {
+    const returnedUser = await AdminModel.create(userData);
+
+    if (!returnedUser) {
+      return res
+        .status(500)
+        .send({
+          success: false,
+          data: { body: null, error: "internal server error" },
+        });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const userData = { email, name, hashedPassword, authentication: {refreshToken: ''} };
+    const { hashedPassword: pass, ...rest } = returnedUser.toObject();
 
-    //check whether user exists
-    const isUserRegistered = await getUserByEmail("Admin", email);
-    if(isUserRegistered || isUserRegistered !== null) {
-        return res.json({success: false, data: {body: null, error: "Already registered"}});
-    }
-
-    //save user to database
-    try {
-        const returnedUser = await AdminModel.create(userData);
-        
-        if(!returnedUser) {
-            return res.status(500).send({success: false, data: {body: null, error: "internal server error"}})
-        };
-
-        const {hashedPassword: pass, ...rest} = returnedUser.toObject();
-
-        return res.json({success: true, data: {body: rest,error: null}});
-
-    } catch (error: any) {
-        return res.json({success: false, data: {body: null, error: error.message}});
-    }
-
-
+    return res.json({ success: true, data: { body: rest, error: null } });
+  } catch (error: any) {
+    return res.json({
+      success: false,
+      data: { body: null, error: error.message },
+    });
+  }
 };
 
 export const createNormalUser = async (req: Request, res: Response) => {
-    const {
-        email,
-        name,
-        password
-    } = req.body;
+  const { email, name, password } = req.body;
 
-    if(!email || !name || !password) {
-        return res.json({success: false, data: {body: null, error: "Missing credentials"}});
+  if (!email || !name || !password) {
+    return res.json({
+      success: false,
+      data: { body: null, error: "Missing credentials" },
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const userData = {
+    email,
+    name,
+    hashedPassword,
+    authentication: { refreshToken: "" },
+  };
+
+  //check whether user exists
+  const isUserRegistered = await getUserByEmail("User", email);
+  if (isUserRegistered || isUserRegistered !== null) {
+    return res.json({
+      success: false,
+      data: { body: null, error: "Already registered" },
+    });
+  }
+
+  try {
+    const returnedUser = await createUser("User", userData);
+
+    if (!returnedUser) {
+      return res
+        .status(500)
+        .send({
+          success: false,
+          data: { body: null, error: "internal server error" },
+        });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const userData = { email, name, hashedPassword, authentication: {refreshToken: ''} };
+    const { hashedPassword: pass, ...rest } = returnedUser.toObject();
 
-    //check whether user exists
-    const isUserRegistered = await getUserByEmail("User", email);
-    if(isUserRegistered || isUserRegistered !== null) {
-        return res.json({success: false, data: {body: null, error: "Already registered"}});
-    };
-
-    try {
-        const returnedUser = await createUser("User", userData);
-        
-        if(!returnedUser) {
-            return res.status(500).send({success: false, data: {body: null, error: "internal server error"}})
-        };
-
-        const {hashedPassword: pass, ...rest} = returnedUser.toObject();
-
-        return res.json({success: true, data: {body: rest,error: null}});
-
-    } catch (error: any) {
-        return res.json({success: false, data: {body: null, error: error.message}});
-    }
-
-
+    return res.json({ success: true, data: { body: rest, error: null } });
+  } catch (error: any) {
+    return res.json({
+      success: false,
+      data: { body: null, error: error.message },
+    });
+  }
 };
 
-export const loginAdmin =  async (req: Request, res: Response) => {
-    const {email, password} = req.body;
-    console.log(email);
-    
-    if(!email || !password) return res.json({success: false, data: {body: null, error: "Please input values"}});
-    
-    //check whether the user exists in database
-    const isUserFound = await AdminModel.findOne({email: email as string});
-    if(!isUserFound) {
-        return res.json({success: false, data: {body: null, error: "User not found in database"}});
+export const loginAdmin = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  console.log(email);
+
+  if (!email || !password)
+    return res.json({
+      success: false,
+      data: { body: null, error: "Please input values" },
+    });
+
+  //check whether the user exists in database
+  const isUserFound = await AdminModel.findOne({ email: email as string });
+  if (!isUserFound) {
+    return res.json({
+      success: false,
+      data: { body: null, error: "User not found in database" },
+    });
+  }
+
+  //check password
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    isUserFound.hashedPassword
+  );
+  if (!isPasswordValid) {
+    return res.json({
+      success: false,
+      data: { body: null, error: "Check password" },
+    });
+  }
+
+  //generate jwt tokens
+  const accessToken = jwt.sign(
+    { id: isUserFound._id, email: isUserFound.email },
+    process.env.ACCESS_TOKEN as string,
+    { expiresIn: "15m" }
+  );
+  const refreshToken = jwt.sign(
+    { id: isUserFound._id, email: isUserFound.email },
+    process.env.REFRESH_TOKEN as string,
+    { expiresIn: "7d" }
+  );
+
+  //update the authentication
+  try {
+    await isUserFound.updateOne({
+      authentication: { refreshToken: refreshToken },
+    });
+    const newUser = await AdminModel.findOne({ email: isUserFound.email });
+    const { hashedPassword: pass, ...rest } = newUser.toObject();
+    //cookie options
+    const options = {
+      httpOnly: true,
+      secure: true,
     };
-    
-    //check password
-    const isPasswordValid = await bcrypt.compare(password, isUserFound.hashedPassword);
-    if(!isPasswordValid) {
-        return res.json({success: false, data: {body: null, error: "Check password"}});
-    };
 
-    //generate jwt tokens
-    const accessToken = jwt.sign({id: isUserFound._id, email: isUserFound.email}, process.env.ACCESS_TOKEN as string, {expiresIn: '15m'});
-    const refreshToken =  jwt.sign({id: isUserFound._id, email: isUserFound.email}, process.env.REFRESH_TOKEN as string, {expiresIn: '7d'});
-
-    //update the authentication
-    try {
-        const updatedUser = await isUserFound.updateOne({authentication: {refreshToken: refreshToken}});
-        const newUser = await AdminModel.findOne({email: isUserFound.email});
-        const {hashedPassword: pass, ...rest} = newUser.toObject();
-
-        return res.json({success: true, data: {body: rest, error: null}});
-
-    } catch (e: any) {
-        throw new Error(e.message);
-    }
-
-
+    return res
+      .cookie("accessToken", accessToken, options)
+      .json({ success: true, data: { body: rest, error: null } });
+  } catch (e: any) {
+    throw new Error(e.message);
+  }
 };
-
