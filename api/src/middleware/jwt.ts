426b0150c01983e78b7e5cf27ffd47027db151cd
Nathami-n <georgenathan010@gmail.com>
2024-06-03 15:42:17 +0300

    Add: extending the Express interface failure

--- api/src/middleware/jwt.ts
+++ api/src/middleware/jwt.ts
@@ -2,7 +2,7 @@ import type {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
 
 export const verifyJWT = (req:Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     if(!token) {
         return res.status(404).json({
@@ -15,10 +15,10 @@ export const verifyJWT = (req:Request, res: Response, next: NextFunction) => {
     };
 
     //decode the token 
-    const verifiedToken = jwt.verify(token, process.env.ACCESS_TOKEN as string);
-
-    //find the user associated with the token 
-    
+    jwt.verify(token, process.env.ACCESS_TOKEN as string, (err, decoded) => {
+        if(err) return res.sendStatus(403);
+        req.
+    } );
 
 
 };
