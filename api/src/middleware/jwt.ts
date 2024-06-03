5cd218ea6c650861306cd5dd889d2a511334c2cc
Nathami-n <georgenathan010@gmail.com>
2024-06-03 16:16:05 +0300

    Added the accessToken validation

--- api/src/middleware/jwt.ts
+++ api/src/middleware/jwt.ts
@@ -1,8 +1,14 @@
 import type {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
 
-export const verifyJWT = (req:Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization.split(" ")[1];
+
+interface CustomRequest extends Request {
+    email: string;
+    id: string;
+}
+
+export const verifyJWT = (req: CustomRequest, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1] || req.headers["authorization"].split("")[1];
 
     if(!token) {
         return res.status(404).json({
@@ -15,10 +21,12 @@ export const verifyJWT = (req:Request, res: Response, next: NextFunction) => {
     };
 
     //decode the token 
-    jwt.verify(token, process.env.ACCESS_TOKEN as string, (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN as string, (err, decoded: {id: string, email: string}) => {
         if(err) return res.sendStatus(403);
-        req.
+        req.email = decoded.email;
+        req.id = decoded.id;
+        next();
     } );
 
-
+   
 };
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import {createAdmin, createNormalUser, loginAdmin} from '../controllers/auth-controllers';
+import { verifyJWT } from '../middleware/jwt';
 
 const authRouter = Router();
 
 authRouter.route('/register/admin').post(createAdmin);
 authRouter.route('/register/user').post(createNormalUser);
-authRouter.route('/login/admin').post(loginAdmin);
+authRouter.route('/login/admin').post(verifyJWT, loginAdmin);
 
 
 export {authRouter};
